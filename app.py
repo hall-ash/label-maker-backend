5179272a47280335814a12c8d16a1c24446325ce
ahall <ahall@inventprise.com>
2024-09-24 11:41:14 -0700

    comment out resources arg in CORS func

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ from api_logic import get_skips_dict
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://192.168.134.118:3000"}})
+#CORS(app, resources={r"/*": {"origins": "http://192.168.134.118:3000"}})
 CORS(app)  # Enable Cross-Origin Resource Sharing (CORS)
-
 
 @app.route('/api/generate_pdf', methods=['POST'])
 def generate_pdf():
@@ -20,31 +19,26 @@ def generate_pdf():
         if not data:
             raise ValueError("No data provided")
 
-
         labels = data.get('labels')
         sheet_type = data.get('sheet_type')
         skip_labels = data.get('skip_labels', None)
         start_label = data.get('start_label', None)
-        border = data.get('border')
-
+        border = data.get('border', False)
+        font_size = data.get('font_size', 12)
+        padding = data.get('padding', 1.75)
+        file_name = data.get('file_name', 'labels')
+        
         input_labels = LabelList(labels).get_label_texts()
 
         used_label_dict = get_skips_dict(skip_labels, sheet_type, start_label) 
-        label_maker = LabelMaker(input_labels=input_labels, used_label_dict=used_label_dict, sheet_type=sheet_type, border=border)
+        label_maker = LabelMaker(input_labels=input_labels, used_label_dict=used_label_dict, sheet_type=sheet_type, border=border, padding_value=padding, font_size=font_size)
         
         pdf_buffer = BytesIO()
-
-        # Path to save the PDF file
-        #file_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'demo.pdf')
-        #label_maker.save(file_path)
         label_maker.save(pdf_buffer)
-
         pdf_buffer.seek(0)
 
-        return send_file(pdf_buffer, as_attachment=True, download_name='demo.pdf', mimetype='application/pdf')
-        
-        # Send the file back to the client
-        #return send_file(file_path, as_attachment=True)
+        return send_file(pdf_buffer, as_attachment=True, download_name=f'{file_name}.pdf', mimetype='application/pdf')
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -52,4 +46,6 @@ def generate_pdf():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host="192.168.134.118", port=5000)
+    host = "192.168.4.112"
+    workhost = "192.168.134.118"
+    app.run(host=workhost, port=5000, debug=True)
